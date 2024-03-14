96471e6da2e717bb8bd3f667dd3a9784f384285c
chris <weavc@pm.me>
2024-03-14 16:40:39 +0000

    unsets fish_greeting variable to disable the help prompt

--- dotfiles/files/.config/fish/conf.d/defaults.fish
+++ dotfiles/files/.config/fish/conf.d/defaults.fish
@@ -3,6 +3,7 @@ if status is-interactive
     set -gx DOTNET_ROOT $HOME/.local/dotnet
     set -gx NVM_DIR $HOME/.nvm
 
+    set -g fish_greeting
     if not test -e $HOME/.config/fish/functions/fisher.fish
         curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
     end
