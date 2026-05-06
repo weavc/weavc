1ef3898b64d38ba4a63fc9e8f9725945be898699
Chris <weavc@pm.me>
2026-05-07 00:06:40 +0100

    Refactor estimation function and baseline calculations
    Refactor estimation logic and improve comments

--- scripts/estimation.py
+++ scripts/estimation.py
@@ -8,11 +8,13 @@ mappings = {
     'XL': 12
 }
 
-# O(n^2) for dev scaling, baseline of 0.1 per change
+# O(n^2) for dev scaling, rate of change 0.1 per dev different from the baseline
 def d(base, devs):
     c = 0.1
     dif = devs-base
     v = (dif**2)*c
+    if dif > 0:
+        v = -v
     return v
 
 # points from ratings
@@ -22,31 +24,35 @@ def r(ratings):
         t += mappings[r]
     return t
 
+devs = [2, 3, 4, 5, 6]
 ratings = ['XL', 'XL', 'L', 'M', 'L', 'L', 'S', 'M', 'M', 'S']
-devs = 7
 
-baselines = [{
-    'ratings': r(['S', 'L', 'S', 'S', 'L', 'S', 'L', 'S']),
-    'weeks': 11,
-    'devs': 4
-},{
-    'ratings': r(['XL', 'L', 'S', 'M', 'M', 'S', 'L', 'S']),
-    'weeks': 11,
-    'devs': 4
-}]
+baselines = [
+    {
+        'ratings': r(['S', 'L', 'S', 'S', 'L', 'S', 'L', 'S']),
+        'weeks': 11,
+        'devs': 4
+    },
+    {
+        'ratings': r(['XL', 'L', 'S', 'M', 'M', 'S', 'L', 'S']),
+        'weeks': 11,
+        'devs': 4
+    }
+]
 
 total = r(ratings)
-results = []
-
-for baseline in baselines:
-    dv = devs-d(devs, baseline['devs'])
-    multiple = total/baseline['ratings']
-    weeks = ((baseline['weeks']*baseline['devs'])/dv)*multiple
-    results.append(weeks)
-    print(f'Comparing: {baseline} ({dv}, {multiple}, {weeks})')
-
 print(f'Points: {total}')
-print(f'Weeks (Upper/Lower/Avg): {math.ceil(max(results))} / {math.ceil(min(results))} / {math.ceil(sum(results, 0)/len(results))}')
+
+for dev in devs:
+    results = []
+    for baseline in baselines:
+        dv = dev+d(baseline['devs'], dev)
+        multiple = total/baseline['ratings']
+        weeks = ((baseline['weeks']*baseline['devs'])/dv)*multiple
+        results.append(weeks)
+        # print(f'Comparing: {baseline} ({dv}, {multiple}, {weeks})')
+    print(f'{dev} Devs, Weeks (Upper/Lower/Avg): {math.ceil(max(results))} / {math.ceil(min(results))} / {math.ceil(sum(results, 0)/len(results))}')
+
 
 
 
