bce3c9936df6ab8930c787d5641ff2d4d51edc1a
Chris <weavc@pm.me>
2026-05-08 08:43:10 +0100

    Refactor ratings handling and mappings structure

--- scripts/estimation.py
+++ scripts/estimation.py
@@ -1,14 +1,8 @@
 import math
 
-mappings = {
-    'XS': 2,
-    'S': 3,
-    'M': 5,
-    'L': 8,
-    'XL': 12
-}
 
 # O(n^2) for dev scaling, rate of change 0.1 per dev different from the baseline
+# The value for c is just a guess but feels about right for variants of 1-3 people
 def d(base, devs):
     c = 0.1
     dif = devs-base
@@ -20,21 +14,36 @@ def d(base, devs):
 # points from ratings
 def r(ratings):
     t = 0
-    for r in ratings:
-        t += mappings[r]
+    for k in ratings.keys():
+        t += ratings[k]*mappings[k]
     return t
 
+# points per size
+mappings = {
+    'XS': 2,
+    'S': 3,
+    'M': 5,
+    'L': 8,
+    'XL': 12
+}
 devs = [2, 3, 4, 5, 6]
-ratings = ['XL', 'XL', 'L', 'M', 'L', 'L', 'S', 'M', 'M', 'S']
+# Sizes
+ratings = {
+    'XS': 0,
+    'S': 2,
+    'M': 3,
+    'L': 3,
+    'XL': 2
+}
 
 baselines = [
     {
-        'ratings': r(['S', 'L', 'S', 'S', 'L', 'S', 'L', 'S']),
+        'ratings': r({'S': 5, 'L': 3}),
         'weeks': 11,
         'devs': 4
     },
     {
-        'ratings': r(['XL', 'L', 'S', 'M', 'M', 'S', 'L', 'S']),
+        'ratings': r({'XL': 1, 'L': 2, 'M': 2, 'S': 3}),
         'weeks': 11,
         'devs': 4
     }
@@ -50,7 +59,6 @@ for dev in devs:
         multiple = total/baseline['ratings']
         weeks = ((baseline['weeks']*baseline['devs'])/dv)*multiple
         results.append(weeks)
-        # print(f'Comparing: {baseline} ({dv}, {multiple}, {weeks})')
     print(f'{dev} Devs, Weeks (Upper/Lower/Avg): {math.ceil(max(results))} / {math.ceil(min(results))} / {math.ceil(sum(results, 0)/len(results))}')
 
 
